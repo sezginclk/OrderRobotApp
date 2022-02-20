100dea1bc56ae9bea4790a60bc1ca0276afd4e51
sezginclk <sezgincolak90@gmail.com>
2022-02-20 20:04:39 +0300

    Service Different Port Op

--- OrderRobot.Operation/Program.cs
+++ OrderRobot.Operation/Program.cs
@@ -20,7 +20,7 @@ namespace OrderRobot.Operation
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://*:5002");
                 });
     }
 }
